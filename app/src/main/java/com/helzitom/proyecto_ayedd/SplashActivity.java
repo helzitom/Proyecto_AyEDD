82c32db61d52024cb4ceca1834e3a75f8216073f
helzitom <helzitom@gmail.com>
2025-09-25 23:57:03 -0500

    Página de login y colores configurados en colors, salto directo de splash a lgin

--- app/src/main/java/com/helzitom/proyecto_ayedd/SplashActivity.java
+++ app/src/main/java/com/helzitom/proyecto_ayedd/SplashActivity.java
@@ -1,6 +1,9 @@
 package com.helzitom.proyecto_ayedd;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -10,10 +13,22 @@ import com.helzitom.proyecto_ayedd.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
+            }
+        },5000);
     }
 }
